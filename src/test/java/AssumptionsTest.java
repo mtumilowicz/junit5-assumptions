f5420955cfcb7b790aab69c6752da5a01caffd13
mtumilowicz <michal.tumilowicz01@gmail.com>
2018-12-05 20:45:06 +0100

    CI assumptions

--- src/test/java/AssumptionsTest.java
+++ src/test/java/AssumptionsTest.java
@@ -3,7 +3,7 @@ import org.junit.Test;
 import java.util.Objects;
 
 import static org.junit.Assert.assertTrue;
-import static org.junit.jupiter.api.Assumptions.assumeFalse;
+import static org.junit.jupiter.api.Assumptions.assumingThat;
 
 /**
  * Created by mtumilowicz on 2018-12-05.
@@ -12,7 +12,7 @@ public class AssumptionsTest {
     
     @Test
     public void createFile_travisSafe() {
-        assumeFalse(() -> Objects.equals(System.getProperty("user.name"), "travis"));
-        assertTrue(false);
+        assumingThat(() -> !Objects.equals(System.getProperty("user.name"), "travis"),
+                () -> assertTrue(false));
     }
 }
